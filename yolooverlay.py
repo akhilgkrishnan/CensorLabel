a69e8f308f86f33497227c99fc8cd881ac6fdf68
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2020-03-17 23:36:51 +0530

    new frame added for gui

--- yolooverlay.py
+++ yolooverlay.py
@@ -1,33 +1,10 @@
-import cv2
-import numpy as np
+import tkinter.scrolledtext as tkscrolled
+import tkinter as tk
 
-# Create a VideoCapture object and read from input file
-# If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('test.mp4')
+default_text = '1234'
+width, height = 20,10
+TKScrollTXT = tkscrolled.ScrolledText(10, width=width, height=height, wrap='word')
 
-# Check if camera opened successfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
-
-# Read until video is completed
-while(cap.isOpened()):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
-
-    # Display the resulting frame
-    cv2.imshow('Frame',frame)
-
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-
-  # Break the loop
-  else: 
-    break
-
-# When everything done, release the video capture object
-cap.release()
-
-# Closes all the frames
-cv2.destroyAllWindows()
+# set default text if desired
+TKScrollTXT.insert(1.0, default_text)
+TKScrollTXT.pack(side=tk.LEFT)
