31df9bd2f83a77dcdf0a5ac6baaa43037ca40787
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2020-03-18 14:55:40 +0530

    Create seperate folder for storing output video and audio

--- yolooverlay.py
+++ yolooverlay.py
@@ -1,10 +1,7 @@
-import tkinter.scrolledtext as tkscrolled
-import tkinter as tk
+path = 'home/sample.mp4'
 
-default_text = '1234'
-width, height = 20,10
-TKScrollTXT = tkscrolled.ScrolledText(10, width=width, height=height, wrap='word')
+akhil = []
 
-# set default text if desired
-TKScrollTXT.insert(1.0, default_text)
-TKScrollTXT.pack(side=tk.LEFT)
+akhil = path.split(".")
+video = akhil[0].split("/")
+print(video[len(video)-1])
